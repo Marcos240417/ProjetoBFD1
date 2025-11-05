14e46ec32049203f31e081c8d826c684c0bab0c8
Marcos Rodrigues Barbosa Terto <mr6227017@gmail.com>
2025-11-05 10:27:19 -0300

    Git BFFD

---
+++ Média.py
@@ -0,0 +1,7 @@
+name = input("Digite seu nome: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"{name}, sua média é: {media:.2f}")
