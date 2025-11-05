53452bc3e89b33a5e7902ec5a429d5dfc6d83e55
Marcos Rodrigues Barbosa Terto <mr6227017@gmail.com>
2025-11-05 11:06:19 -0300

    atual

--- Média.py
+++ Média.py
@@ -5,3 +5,10 @@ nota2 = float(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
 
 print(f"{name}, sua média é: {media:.2f}")
+
+if media >= 7:
+    print(f"{name} está aprovado(a)")
+elif media >= 6 and media >=5:
+    print(f"{name} está em recuperação")
+else:
+    print(f"{name} está reprovado(a)")
